e9154b70e9a8a5eeaf5cd38ee0125852a07525f7
Lucas Gilardi <lcsgilardi@gmail.com>
2021-10-19 10:44:03 -0300

    Agregado de NavBar y Home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div>
+                <h2>MADE FOR <br/>COMFORT</h2>
+                <p className="descriptionTitle">Explore our breezy summer collection now</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <header>
+            <div>
+                <h1>Arivaci &amp; Co.</h1>
+            </div>
+            <div>
+                <nav>
+                    <ul>
+                        <li><a href="#home">HOME</a></li>
+                        <li><a href="#collection">COLLECTION</a></li>
+                        <li><a href="#shop">SHOP</a></li>
+                        <li><a href="#contact">CONTACT US</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default NavBar
